7eb6f56d7d91e59d0a476dfa18c8c2cfc34b82c1
maru <wsj7178@gmail.com>
2018-08-02 23:18:54 +0900

    bug fix

--- src/mskyblock/event/SkyblockGiveEvent.java
+++ src/mskyblock/event/SkyblockGiveEvent.java
@@ -2,9 +2,15 @@ package mskyblock.event;
 
 import cn.nukkit.Player;
 import cn.nukkit.event.Cancellable;
+import cn.nukkit.event.HandlerList;
 import mskyblock.skyblock.Skyblock;
 
 public class SkyblockGiveEvent extends SkyblockEvent implements Cancellable {
+	private static final HandlerList handlers = new HandlerList();
+	
+	public static HandlerList getHandlers() {
+	    return handlers;
+	}
 	private Player player;
 	
 	public SkyblockGiveEvent(Skyblock skyblock, Player player) {
